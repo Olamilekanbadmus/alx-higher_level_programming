3bc2b58c58ef578fc290dbdac1a3407684e891d6
olamilekanbadmus <oluwaginggerayomi@gmail.com>
2022-11-29 11:36:24 -0800

    int

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-for i in range(0, 10):
-    for j in range((i+1), 10):
-        if (i != 8) or (j != 9):
-            print("{i}{j}, ".format(i, j), end="")
-        else:
-            print(f"{i}{j}".format(i, j))
