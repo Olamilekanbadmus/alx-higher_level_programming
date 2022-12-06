5cb3422359ee78ef875ace5f42663c97ca01b614
olamilekanbadmus <oluwaginggerayomi@gmail.com>
2022-12-06 02:26:51 -0800

    commit

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -3,10 +3,8 @@ def divisible_by_2(my_list=[]):
     if my_list:
         values = []
         for i in my_list:
-            if i % 2 is 0:
+            if i % 2 == 0:
                 values.append(True)
             else:
                 values.append(False)
-        return values
-
-# return [(i % 2 == 0) for i in my_list]
+        return values
